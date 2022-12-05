e22afc3a80c23500e9ee694d58e548286a03a94f
faisal ahmad <faisalahmad595@gmail.com>
2022-12-05 12:20:21 +0530

    first commit

---
+++ frontend/mernapp/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>This is to-do-list app you can use for fill your form</div>
+  )
+}
+
+export default About
---
+++ frontend/mernapp/src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>if you need help then contact me-00000000</div>
+  )
+}
+
+export default Contact
